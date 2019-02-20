ce24c648cd893263de03474671f91e5525422153
“wang” <940831020@qq.com>
2019-02-21 00:14:03 +0800

    Initial commit 20190221

---
+++ src/main/java/com/imooc/o2o/entity/PersonInfo.java
@@ -0,0 +1,97 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+public class PersonInfo {
+//    用户id
+    private Long userId;
+//    姓名
+    private String username;
+//    性别
+    private String gender;
+//    邮箱
+    private String email;
+//    头像图片的地址
+    private String profileImg;
+//    用户状态：可用或者禁用
+    private Integer enableStatus;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImg() {
+        return profileImg;
+    }
+
+    public void setProfileImg(String profileImg) {
+        this.profileImg = profileImg;
+    }
+
+    public Integer getEnableStatus() {
+        return enableStatus;
+    }
+
+    public void setEnableStatus(Integer enableStatus) {
+        this.enableStatus = enableStatus;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    //    用户类型:1.顾客 2.店家 3.超级管理员
+    private Integer userType;
+//    创建时间
+    private Date createTime;
+//    更新时间
+    private Date lastEditTime;
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%-- 下面这行代码是解决中文乱码的--%>
+<%-- maven生成的index.jsp文件没有下面这行代码，通过在webapp下新建一个任意的jsp文件，将这行代码复制过来即可--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2>Hello World!你好，maven</h2>
+</body>
+</html>
